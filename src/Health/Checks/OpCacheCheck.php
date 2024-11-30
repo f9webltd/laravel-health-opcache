a14a6fa3fd9a14f4256f77caed2d8d56fe2df0d8
Robert Allport <rob@f9web.co.uk>
2024-11-30 14:27:43 +0000

    Tighten OPcache Check

--- src/Health/Checks/OpCacheCheck.php
+++ src/Health/Checks/OpCacheCheck.php
@@ -14,17 +14,43 @@ class OpCacheCheck extends Check
 {
     public function run(): Result
     {
+        $this->label('OPcache');
+        
         $result = Result::make();
 
         if (false === $this->checkOpcacheIsRunning()) {
-            return $result->failed('Opcache is not running');
+            return $result->failed('OPcache is not running');
         }
 
-        return $result->ok()->shortSummary('Opcache is running');
+        return $result->ok()->shortSummary('OPcache is running');
     }
 
     private function checkOpcacheIsRunning(): bool
     {
-        return function_exists('opcache_get_status') && is_array(opcache_get_status());
+        if (
+            !function_exists('opcache_get_status') ||
+            !function_exists('opcache_get_configuration') ||
+            !is_array(opcache_get_status()) ||
+            !is_array(opcache_get_configuration())
+        ) {
+            return false;
+        }
+
+        $config = opcache_get_configuration();
+        $status = opcache_get_status();
+
+        if (
+            (false === ($config['directives']['opcache.enable'] ?? false)) ||
+            (false === ($status['opcache_enabled'] ?? false))
+        ) {
+            return false;
+        }
+
+        // sanity check - ensure scripts are being stored within memory
+        if (($status['memory_usage']['used_memory'] ?? -1) < 1) {
+            return false;
+        }
+
+        return true;
     }
 }
